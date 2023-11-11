bec1424ea62ed713d8dd195cfd3bc733f2672f09
Mahrus <mahrusrama7@gmail.com>
2023-11-11 08:32:07 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -337,7 +337,7 @@
                         </script>,
                         made with <i class="fa fa-heart"></i> by
                         <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                        for a better web.
+                        for a better webs
                     </div>
                 </div>
                 <div class="col-lg-6">
